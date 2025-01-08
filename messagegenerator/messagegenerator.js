6df5b33248183dcef4fe43bdde201178fcd88ca9
danielcotsfdz@gmail.com <danielcotsfdz@gmail.com>
2025-01-08 15:15:54 +0000

    frases incluídas en array

---
+++ messagegenerator/messagegenerator.js
@@ -0,0 +1,9 @@
+const frasesmodelo() {
+    let lineas = ["Solo con esfuerzo y consistencia conseguirás cumplir tus objetivos. No con negatividad.",
+                  "No pierdas tiempo siendo negativo. Sé productivo. Pues de ello vendrá tu cambio." , 
+                  "Tus sueños se cumplen con trabajo, no con culpa.",
+                  "Que sea difícil no significa que sea imposible, que sea imposible no significa que vayas a rendirte.",
+                  "Tu felicidad depende de la calidad de tus pensamientos"]
+};
+
+const randomNumber = getRandomNumber(1, lineas.lenght);
