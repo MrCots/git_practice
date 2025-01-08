59131d461e9d8ade3d1011302517f7204c6f9d54
danielcotsfdz@gmail.com <danielcotsfdz@gmail.com>
2025-01-08 15:28:37 +0000

    corregido error en funcion javascript.

--- messagegenerator/messagegenerator.js
+++ messagegenerator/messagegenerator.js
@@ -1,17 +1,18 @@
-const frasesmodelo() {
+function frasesmodelo() {
     let lineas = ["Solo con esfuerzo y consistencia conseguirás cumplir tus objetivos. No con negatividad.",
-                  "No pierdas tiempo siendo negativo. Sé productivo. Pues de ello vendrá tu cambio." , 
+                  "No pierdas tiempo siendo negativo. Sé productivo. Pues de ello vendrá tu cambio.", 
                   "Tus sueños se cumplen con trabajo, no con culpa.",
                   "Que sea difícil no significa que sea imposible, que sea imposible no significa que vayas a rendirte.",
-                  "Tu felicidad depende de la calidad de tus pensamientos"]
+                  "Tu felicidad depende de la calidad de tus pensamientos"];
 
+    const randomNumber = getRandomNumber(1, lineas.length);
+    const randomPhrase = lineas[randomNumber - 1];
 
-const randomNumber = getRandomNumber(1, lineas.lenght);
-const randomPhrase = lineas[randomNumber - 1];
-
-return `Número aleatorio: ${randomNumber}, Frase: "${randomPhrase}"`;
+    return `Número aleatorio: ${randomNumber}, Frase: "${randomPhrase}"`;
 }
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+console.log(frasesmodelo());
